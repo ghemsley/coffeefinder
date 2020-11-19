7c0bc49de82a0ef091703e5ef85530985fdeb263
ghemsley <ghemsley@protonmail.ch>
2020-11-19 09:52:31 -0600

    Initial commit

---
+++ lib/coffeefinder.rb
@@ -0,0 +1,6 @@
+require "coffeefinder/version"
+
+module Coffeefinder
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/coffeefinder/version.rb
@@ -0,0 +1,3 @@
+module Coffeefinder
+  VERSION = "0.1.0"
+end
